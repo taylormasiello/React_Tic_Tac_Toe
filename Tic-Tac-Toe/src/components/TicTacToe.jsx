cfb99438edfb7ac9427181d97519ce0291884baf
Taylor Masiello <taylor.masiello@outlook.com>
2025-01-07 14:09:10 -0500

    Basic Game Components Template

---
+++ Tic-Tac-Toe/src/components/TicTacToe.jsx
@@ -0,0 +1,12 @@
+import Board from "./Board";
+
+function TicTacToe() {
+    return(
+        <div>
+        <h1>Tic Tac Toe</h1>
+        <Board />
+    </div>
+    )
+}
+
+export default TicTacToe;
